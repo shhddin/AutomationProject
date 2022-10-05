7efc0a8175c44280368839ed62d2059fee457f6e
shhddin <shhdddin@gmail.com>
2022-10-04 22:23:49 -0400

    groups

--- src/test/java/calculator/AutoCalculator.java
+++ src/test/java/calculator/AutoCalculator.java
@@ -4,31 +4,13 @@ import org.testng.annotations.Test;
 
 import base.BaseClass;
 
-public class AutoCalculator extends BaseClass {
+public class AutoCalculatorGrouping extends BaseClass {
 
-	@Test(groups = "own")
-	public void autoCalculatorOwn() {
+	@Test(enabled = true)
+	public void autoCalculator() {
 		homePage.homepageCalculate();
 		calculate.calculateSteps1("Car Insurance Coverage Calculator | Liberty Mutual",
 				"Car insurance coverage calculator", "19111", true, false, false, true, false, false);
-		calculate.calculateSteps2();
-		calculate.calculateSteps3And4();
-	}
-
-	@Test(groups = "lease")
-	public void autoCalculatorLease() {
-		homePage.homepageCalculate();
-		calculate.calculateSteps1("Car Insurance Coverage Calculator | Liberty Mutual",
-				"Car insurance coverage calculator", "19111", false, true, false, true, false, false);
-		calculate.calculateSteps2();
-		calculate.calculateSteps3And4();
-	}
-
-	@Test(groups = "finance")
-	public void autoCalculatorFinance() {
-		homePage.homepageCalculate();
-		calculate.calculateSteps1("Car Insurance Coverage Calculator | Liberty Mutual",
-				"Car insurance coverage calculator", "19111", false, true, false, true, false, false);
 		calculate.calculateSteps2();
 		calculate.calculateSteps3And4();
 	}
